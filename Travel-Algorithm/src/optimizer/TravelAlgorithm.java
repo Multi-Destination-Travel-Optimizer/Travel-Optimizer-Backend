5906168058d03d701dd82ed88480647e918177c3
Eric Jin <ericjin2@illinois.edu>
2020-08-19 20:52:35 -0700

    Hide API Key

--- Travel-Algorithm/src/optimizer/TravelAlgorithm.java
+++ Travel-Algorithm/src/optimizer/TravelAlgorithm.java
@@ -1,17 +1,12 @@
 package optimizer;
 
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import com.mashape.unirest.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Handles searching for the optimum sequence of tickets based on air fare.
@@ -21,6 +16,12 @@ public class TravelAlgorithm {
      * All the responses from API calls to Skycanner Flight Search.
      */
     private ArrayList<HttpResponse<String>> responses;
+    /**
+     *
+     */
+    private String skyscannerAPIKey;
+
+    //String SYSCANNER_API_KEY = process.env.SKYSCANNER_API_KEY;
 
     /**
      * Finds the optimum airplane tickets from the user's choice of the sequence of destination by comparing ticket
@@ -34,19 +35,23 @@ public class TravelAlgorithm {
     public ArrayList<AirTicket> findOptimumTickets(ArrayList<String> destinations,
                                                    String tripStartDate,
                                                    String tripEndDate) throws IOException, UnirestException {
+        Map<String, String> env = System.getenv();
+        skyscannerAPIKey = env.get("SKYSCANNER_API_KEY");
+
         responses = new ArrayList<>();
 
         // There is one airplane flight for ever two destinations (e.g. 3 destinations, 2 flights).
         for (int i = 0; i < destinations.size() - 1; i++) {
             String startIATACode = destinations.get(i);
             String endIATACode = destinations.get(i + 1);
-            //TODO: Change start and end dates for each call.
+
             HttpResponse<String> response = Unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com" +
                     "/apiservices/browsequotes/v1.0/US/USD/en-US/" +
                     startIATACode + "-sky/" + endIATACode + "-sky/2020-09-01?inboundpartialdate=2019-12-01")
                     .header("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
-                    .header("x-rapidapi-key", "0f696ca81amsh30ca5205dc3bcbfp1b543bjsn619f9aae24a8")
+                    .header("x-rapidapi-key", skyscannerAPIKey)
                     .asString();
+            responses.add(response);
         }
 
         return null;
