ed0ac8de3fdff45f706e787825345471a31deeab
Eric Jin <ericjin2@illinois.edu>
2020-08-19 21:54:42 -0700

    Correctly Gets API Key

--- Travel-Algorithm/src/main/java/optimizer/TravelAlgorithm.java
+++ Travel-Algorithm/src/main/java/optimizer/TravelAlgorithm.java
@@ -3,6 +3,7 @@ package optimizer;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,11 +18,9 @@ public class TravelAlgorithm {
      */
     private ArrayList<HttpResponse<String>> responses;
     /**
-     *
+     * API Key used to get requests for Skyscanner.
      */
     private String skyscannerAPIKey;
-
-    //String SYSCANNER_API_KEY = process.env.SKYSCANNER_API_KEY;
 
     /**
      * Finds the optimum airplane tickets from the user's choice of the sequence of destination by comparing ticket
@@ -33,25 +32,31 @@ public class TravelAlgorithm {
      * @return ArrayList of the cheapest AirTickets within the user's journey duration.
      */
     public ArrayList<AirTicket> findOptimumTickets(ArrayList<String> destinations,
-                                                   String tripStartDate,
-                                                   String tripEndDate) throws IOException, UnirestException {
-        Map<String, String> env = System.getenv();
-        skyscannerAPIKey = env.get("SKYSCANNER_API_KEY");
+                                                   String tripStartDate, String tripEndDate) throws IOException, UnirestException {
 
-        responses = new ArrayList<>();
+        Dotenv dotenv = Dotenv.configure()
+                .directory("Travel-Algorithm/.env")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        skyscannerAPIKey = dotenv.get("SKYSCANNER_API_KEY");
+
+        responses = new ArrayList<HttpResponse<String>>();
 
         // There is one airplane flight for ever two destinations (e.g. 3 destinations, 2 flights).
         for (int i = 0; i < destinations.size() - 1; i++) {
             String startIATACode = destinations.get(i);
             String endIATACode = destinations.get(i + 1);
 
-            HttpResponse<String> response = Unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com" +
-                    "/apiservices/browsequotes/v1.0/US/USD/en-US/" +
+            HttpResponse<String> response = Unirest.get("https://skyscanner-skyscanner-flight-search-v1.p." +
+                    "rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" +
                     startIATACode + "-sky/" + endIATACode + "-sky/2020-09-01?inboundpartialdate=2019-12-01")
                     .header("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
                     .header("x-rapidapi-key", skyscannerAPIKey)
                     .asString();
             responses.add(response);
+            System.out.println(response.getBody());
         }
 
         return null;
